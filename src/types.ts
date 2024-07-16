a7692fefccf81064f6539ce005ebfd3d20930fe8
Simon Lund <simon97lund@gmail.com>
2024-07-17 00:44:26 +0200

    :poop: remove type check

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
 import { type InferType, object, string } from 'yup';
 
+
 export const ErrorEntry = object({
 	projectStage: string().required(),
-	topic: string().required(),
-	qrp: string(),
-	honestError: string(),
+	// TODO: fix type check here
+	topic: string(), //.required(),
+	qrp: string().nullable(),
+	honestError: string().nullable()
 });
 
 export type TErrorEntry = InferType<typeof ErrorEntry>;
