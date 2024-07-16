ae4030f0a614985106cf12cb014377857fbdb005
Simon Lund <simon97lund@gmail.com>
2024-07-17 00:07:11 +0200

    :bug: fix syntax. paths is object with "base" key

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: 'https://simon-lund.github.io'
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
