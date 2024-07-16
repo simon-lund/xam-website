dc0ec060865b01931b30887c8489100bb539c4c9
Simon Lund <simon97lund@gmail.com>
2024-07-17 00:38:01 +0200

    :wrench: load data from csv

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,10 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+	declare module "*.csv" {
+		export default <{ [key: string]: string }>Array;
+	}
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,17 @@
-import type { TErrorEntry } from '../types';
-import data from '../assets/errors.json';
+import { ErrorEntry, type TErrorEntry } from '../types';
+import data from '../assets/Error_Culture_Community_Collection.csv';
 
+function transform(entry: Record<string, string>) {
+	return ErrorEntry.cast({
+		projectStage: entry['Project stage'],
+		topic: entry['Topic / field'],
+		qrp: entry['Questionable Research Practices (QRP)'],
+		honestError: entry['Honest error'],
+	});
+}
 
 export async function load() {
-	const errorEntries: TErrorEntry[] = data;
+	const errorEntries: TErrorEntry[] = data.map(transform);
+
 	return { errorEntries };
 }
