5b00cda0425b5445a01246b9f413448ec6a60f62
Simon Lund <simon97lund@gmail.com>
2024-07-17 00:48:19 +0200

    :poop: re-add strict types but disable casting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,14 @@
-import { ErrorEntry, type TErrorEntry } from '../types';
+import { type TErrorEntry } from '../types';
 import data from '../assets/Error_Culture_Community_Collection.csv';
 
 function transform(entry: Record<string, string>) {
-	return ErrorEntry.cast({
+	// TODO: add type checking with casting
+	return {
 		projectStage: entry['Project stage'],
 		topic: entry['Topic / field'],
 		qrp: entry['Questionable Research Practices (QRP)'],
 		honestError: entry['Honest error'],
-	});
+	};
 }
 
 export async function load() {
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,8 @@
 import { type InferType, object, string } from 'yup';
 
-
 export const ErrorEntry = object({
 	projectStage: string().required(),
-	// TODO: fix type check here
-	topic: string(), //.required(),
+	topic: string().required(),
 	qrp: string().nullable(),
 	honestError: string().nullable()
 });
