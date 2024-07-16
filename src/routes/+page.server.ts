37e40b06c75cabc3a03bc25aef2700366a6b6220
Simon Lund <simon97lund@gmail.com>
2024-07-16 23:49:48 +0200

    :wrench: configure for static site rendering

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,8 @@
 import type { TErrorEntry } from '../types';
 import data from '../assets/errors.json';
 
+
 export async function load() {
-
-
 	const errorEntries: TErrorEntry[] = data;
 	return { errorEntries };
 }
